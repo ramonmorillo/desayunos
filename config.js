27f6a3e74772ed66a11b7701bdf0e64dfbd99056
ramonmorillo <ramon.morillo.verdugo@gmail.com>
2026-04-10 12:24:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: https://wbpguvsenoenldidoicp.supabase.co
-  SUPABASE_ANON_KEY: sb_publishable_ZJLIxsn0OqNDHlmrHRwUzg_S14FrbJC
+  SUPABASE_URL: "https://wbpguvsenoenldidoicp.supabase.co"
+  SUPABASE_ANON_KEY: "sb_publishable_ZJLIxsn0OqNDHlmrHRwUzg_S14FrbJC"
 };
